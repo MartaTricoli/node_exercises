c0a46d8362d952e6db2488362ece505091abe2df
Marta <martatricoli42@gmail.com>
2024-02-16 13:00:58 +0100

    add error control on user.js

--- commonJsModules/user.js
+++ commonJsModules/user.js
@@ -1,4 +1,10 @@
 const user = (name, age) => {
+    if (typeof name !== "string" || name === "") {
+        throw new Error("name must be a valid string");
+    }
+    if (typeof age !== "number" || isNaN(age)) {
+        throw new Error("age must be a valid number");
+    }
     console.log(`my name is: ${name}, I am ${age} years old.`) ;
 }
 
