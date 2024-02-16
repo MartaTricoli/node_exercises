76b385bdd0115a2de861e50a4d7bbf7dab92cd4a
Marta <martatricoli42@gmail.com>
2024-02-16 12:55:06 +0100

    add commonJsModules directory

---
+++ commonJsModules/hello.js
@@ -0,0 +1,3 @@
+const user = require("./user.js");
+
+user("Marta", 27);
---
+++ commonJsModules/user.js
@@ -0,0 +1,5 @@
+const user = (name, age) => {
+    console.log(`my name is: ${name}, I am ${age} years old.`) ;
+}
+
+module.exports = user;
