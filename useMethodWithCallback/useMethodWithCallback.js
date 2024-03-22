34d979632586f84e691a5be41e83868009601a09
Marta <martatricoli42@gmail.com>
2024-03-22 15:41:25 +0100

    fix useMethodWithCallback.js

--- useMethodWithCallback/useMethodWithCallback.js
+++ useMethodWithCallback/useMethodWithCallback.js
@@ -1,11 +1,12 @@
-const fs = require("node:fs");
+import * as fs from "node:fs";
 
 const data = "Quando sognerai di un mondo che non è mai esistito o di uno che non esisterà mai e in cui sei di nuovo felice, vorrà dire che ti sei arreso. Capisci? E tu non ti puoi arrendere. Io non te lo permetterò."
 
-fs.writeFile("./prova.txt", data, error => {
-    if (error) {
-        throw new Error("Error occurred!")
-    }
+fs.writeFile("prova.txt", data, { encoding: "utf-8" }, error => {
+   if (error) {
+      console.error(error);
+      return;
+   }
 
-    console.log("file updated!")
-})
+   console.log("File has been written successfully!");
+});
